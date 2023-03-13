fb5b9593ce1ab32af9f5b2d5317a34e6c4c68996
George Samuel <geosam2708@gmail.com>
2023-03-13 09:26:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name : "Sam",
+    streetAddress: "11 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const anotherObject = {...employee};
+    anotherObject[key] = value;
+    return anotherObject
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]= value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const anotherObject = {...employee};
+    delete anotherObject[key];
+    return anotherObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+}
